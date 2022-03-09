b5c8f0d8340659619a4b4b3159f8d7e7c50d5370
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-09 13:50:05 +0100

    March 9th

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Header.css';
 
+import {FiShoppingCart} from 'react-icons/fi';
+
 function Header() {
     return (
         <div className='Header'>
-            
+            <div className='H_d1'>
+                <p>Dr <span>Food</span></p>
+            </div>
+            <div className='H_d2'>
+                <FiShoppingCart size={25} color='#ED7C0B' />
+            </div>
         </div>
     );
 }
--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './Home.css';
 
 import {Header} from '../../Components'
+import Intro from './Components/Intro';
 
 function Home() {
     return (
         <div className='Home'>
             <Header />
+            <Intro />
         </div>
     );
 }
