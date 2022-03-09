c28f3cb137af31744ae6c9cc45313240fe7ffa42
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-09 17:34:09 +0100

    March 9th

--- src/Screens/Selected/Selected.tsx
+++ src/Screens/Selected/Selected.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Selected.css';
 
+import chicken from '../../Assets/chicken.jpg';
+
 const Selected = () => {
     return (
         <div className='SelectedPage'>
-            
+            <div className='SelectedPageTop' style={{backgroundImage: `url(${chicken})`}} ></div>
+            <div className='SelectedPageBottom'></div>
         </div>
     );
 }
