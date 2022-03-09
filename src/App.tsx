4cb7ad3cc58100dc222b16a964536d16dd319811
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-09 13:30:23 +0100

    March 9th

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import {Home} from './Screens';
+
 function App(props:any) {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
