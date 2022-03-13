168c6c92d521d564762682562676532785313b05
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-13 15:52:46 +0100

    March 13th

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 
-import {FiShoppingCart} from 'react-icons/fi';
+import {FiBell} from 'react-icons/fi';
 
 function Header(props: any) {
     return (
@@ -9,9 +9,9 @@ function Header(props: any) {
             <div className='H_d1'>
                 <p>Dr <span>Food</span></p>
             </div>
-            {/* <div className='H_d2'>
-                <FiShoppingCart size={25} color='#ED7C0B' onClick={() => props.props.history.push('/cart')} />
-            </div> */}
+            <div className='H_d2'>
+                <FiBell size={25} color='#ED7C0B' onClick={() => props.props.history.push('/cart')} />
+            </div>
         </div>
     );
 }
