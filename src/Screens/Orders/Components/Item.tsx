c5b78dd28c348b9634422fe3a589ada45cc2e149
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-13 16:32:15 +0100

    March 13th

--- src/Screens/Orders/Components/Item.tsx
+++ src/Screens/Orders/Components/Item.tsx
@@ -8,7 +8,11 @@ function Item() {
             <p className='OR_title' >Ifeanyi Ahumareze, 12 march 2021</p>
             <p className='OR_details'>0802318898</p>
             <p className='OR_details'>12 bishops court, owerri</p>
-            <div className='OR_main'></div>
+            <div className='OR_main'>
+                <p className='OR_Item' >Potato fries x2 @N3000</p>
+                <p className='OR_Item' >Potato fries x2 @N3000</p>
+            </div>
+            <div className='OR_Bottom'></div>
         </div>
     );
 }
