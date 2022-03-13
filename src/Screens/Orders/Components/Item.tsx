ee337bbf22afc5c1cf91b12e1a97e4392440f695
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-13 16:24:29 +0100

    March 13th

--- src/Screens/Orders/Components/Item.tsx
+++ src/Screens/Orders/Components/Item.tsx
@@ -5,7 +5,10 @@ import { width } from '../../../config/dimensions';
 function Item() {
     return (
         <div className='OrderItem' style={{width: width - 20}}>
-            
+            <p className='OR_title' >Ifeanyi Ahumareze, 12 march 2021</p>
+            <p className='OR_details'>0802318898</p>
+            <p className='OR_details'>12 bishops court, owerri</p>
+            <div className='OR_main'></div>
         </div>
     );
 }
