c61f12fc33efd19216144694096e3ea377527237
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-16 20:20:54 +0100

    March 16th

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 //Imported packages
 import {Redirect, Route, Switch} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 //Imported Packages
 import {Home, Auth, NewFood, Orders} from './Screens';
+import * as actions from './Redux/actions/actions';
 
 function App(props:any) {
-  return (
-    <div className="App">
+
+  useEffect(() => {
+    props.signup()
+  }, [])
+
+  let view = (
+    <Switch>
+      <Route path='/auth' component={Auth} />
+      <Redirect to='/auth' />
+    </Switch>
+  )
+
+  if(props.token){
+    view = (
       <Switch>
         <Route path='/' component={Home} exact />
-        <Route path='/auth' component={Auth} />
         <Route path='/orders' component={Orders} />
         <Route path='/new' component={NewFood} />
         <Redirect to='/' />
       </Switch>
+    )
+  }
+
+  return (
+    <div className="App">
+      {view}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state: any) => {
+  return{
+    token: state.token
+  }
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return{
+    init: () => dispatch(actions.init),
+    signup: () => dispatch(actions.signUp)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/Redux/actions/actions.ts
+++ src/Redux/actions/actions.ts
@@ -1,1 +1,28 @@
 import * as actionTypes from './actionTypes';
+
+const init = () => {
+    return (dispatch: any) => {
+        const arr = localStorage.getItem('@dr_foodCart');
+        console.log(arr)
+    }
+};
+
+const signUp = () => {
+    console.log('dispatchhe')
+    return (dispatch: any) => {
+        console.log('dispatchhe')
+    }
+}
+
+const setToken = (e: string) => {
+    return{
+        type: actionTypes.SETTOKEN,
+        value: e
+    }
+}
+
+export {
+    init,
+    setToken,
+    signUp
+}
